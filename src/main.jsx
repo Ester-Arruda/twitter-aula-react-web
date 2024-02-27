19f0ce972a7bb51528966209caee2cee3c869cac
egvelho <eduardo.velho@icloud.com>
2024-02-26 22:55:22 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,5 @@ import "@fontsource/roboto/700.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
