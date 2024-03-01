193149daf878ee7025d18fd00d449a968483f758
egvelho <eduardo.velho@icloud.com>
2024-03-01 03:18:03 -0300

    updates

--- src/layout/AppBar.jsx
+++ src/layout/AppBar.jsx
@@ -1,27 +1,3 @@
-import MuiAppBar from "@mui/material/AppBar";
-import Container from "@mui/material/Container";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import AdbIcon from "@mui/icons-material/Adb";
-
 export function AppBar() {
-  return (
-    <header>
-      <MuiAppBar position="sticky">
-        <Container>
-          <Toolbar disableGutters>
-            <AdbIcon />
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-            >
-              Paçaro
-            </Typography>
-          </Toolbar>
-        </Container>
-      </MuiAppBar>
-    </header>
-  );
+  return <header>Header</header>;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,8 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/fira-code";
+import "@fontsource/fira-sans";
+import "./main.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div>
-    <App />
-    <CssBaseline />
-  </div>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
