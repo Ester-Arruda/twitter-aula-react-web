3a15aef7b43d134f2db166971bc86340c0b7c320
egvelho <eduardo.velho@icloud.com>
2024-03-02 14:17:31 -0300

    Dcors

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { usersRouter } from "./users.router.js";
 
 export const api = express();
 const router = Router();
 
+api.use(cors());
 api.use(express.json());
 api.use(bodyParser.json());
 
